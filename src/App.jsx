50459e561837d41d81b267958c0ec57326e3a9b1
Numan <nomanmani62@gmail.com>
2022-10-30 02:32:14 +0500

    created new branch for creating grid layouts

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import Hero from './components/Hero';
 import InfoSection from './components/InfoSection';
 import MusicSection from './components/MusicSection';
 import Navbar from './components/Navbar';
+import PromotionSection from './components/PromotionSection';
+import GridSection from './components/GridSection';
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <MusicSection />
-      <InfoSection />
+      {/* <Navbar /> */}
+      {/* <Hero /> */}
+      {/* <MusicSection /> */}
+      {/* <InfoSection /> */}
+      {/* <PromotionSection /> */}
+      <GridSection />
     </>
   );
 }
