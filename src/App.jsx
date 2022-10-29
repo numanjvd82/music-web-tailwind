b678d76986fbd49e34d84011d578f9d30a54d5cd
Numan <nomanmani62@gmail.com>
2022-10-29 19:39:16 +0500

    init repo with tailwind css with some settings

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from './components/Hero';
+import MusicSection from './components/MusicSection';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <MusicSection />
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        navColor: '#270082',
+        btnColor: '#DC0000',
+        footerColor: '#1B0058',
+      },
+      backgroundImage: {
+        'hero-img': "url('./assets/images/bg-1.png')",
+      },
+    },
+  },
+  plugins: [],
+};
